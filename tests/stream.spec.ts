16a9482387a282d78a6013e24797cad181b67d9f
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2023-02-15 21:59:17 +0200

    Created dummy tests

---
+++ tests/stream.spec.ts
@@ -0,0 +1,13 @@
+describe('RuuviInfluxTransform', () => {
+  it('Defined Ruuvi values are inserted into Influx fields (excl. "id" + "mac")', () => {
+    // TODO
+  })
+
+  it('Ruuvi Bluetooth peripheral ID and name are inserted into Influx tags', () => {
+    // TODO
+  })
+
+  it('Ruuvi measurement is only transformed once ("measurementSequence")', () => {
+    // TODO
+  })
+})
